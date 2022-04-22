e5618128f05a82f6329c1c80a197f273cc623d8f
Artumy <Artumy70@gmail.com>
2022-04-22 13:43:58 +0300

    Fix LineRender texture

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -6,17 +6,17 @@ public class EndGame : MonoBehaviour
     private Cell[] _cells;
     private void Start()
     {
-        InvokeRepeating("CheckCell", 5, 3);
+        InvokeRepeating("CheckCell", 5f, 3f);
         _cells = FindObjectsOfType<Cell>();
     }
 
     private void CheckCell()
     {
         var capturedCells = _cells.Where(cell => cell.Type != Cell.CellType.None).ToArray();
-        for(int i = 0; i < capturedCells.Length - 1; i++)
-        {           
-            if(capturedCells[i].Type != capturedCells[i+1].Type)
-                return;            
+        for (int i = 0; i < capturedCells.Length - 1; i++)
+        {
+            if (capturedCells[i].Type != capturedCells[i + 1].Type)
+                return;
         }
 
         FinishGame();
