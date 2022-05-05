58977a9121f177b9ebd5ee1455431a7a686f1608
Artumy <Artumy70@gmail.com>
2022-05-06 01:21:11 +0300

    Fixed recordSettings

--- Assets/Scripts/Levels/LevelTimer.cs
+++ Assets/Scripts/Levels/LevelTimer.cs
@@ -7,8 +7,28 @@ public class LevelTimer : MonoBehaviour
 {
     private void Start()
     {
-        PlayerPrefs.SetInt("Level" + (SceneManager.GetActiveScene().buildIndex - 1) + "Second", (int)Time.timeSinceLevelLoad % 60);
-        PlayerPrefs.SetInt("Level" + (SceneManager.GetActiveScene().buildIndex - 1) + "Minute", (int)Time.timeSinceLevelLoad / 60);
+        int _levelNumber = SceneManager.GetActiveScene().buildIndex - 1;
+        var cell = FindObjectsOfType<Cell>();
+        for (int i = 0; i < cell.Length; i++)
+        {
+            if (cell[i].Type == Cell.CellType.Enemy) return;
+        }
+        if (Records.Record[_levelNumber].Minute >= (int)Time.timeSinceLevelLoad / 60)
+        {
+            if (Records.Record[_levelNumber].Second >= (int)Time.timeSinceLevelLoad % 60)
+            {
+                SaveRecords();
+            }
+        }
+        if (Records.Record[_levelNumber].Second == 0 && Records.Record[_levelNumber].Minute == 0)
+            SaveRecords();
+    }
+
+    private void SaveRecords()
+    {
+        int _levelNumber = SceneManager.GetActiveScene().buildIndex - 1;
+        PlayerPrefs.SetInt("Level" + _levelNumber + "Second", (int)Time.timeSinceLevelLoad % 60);
+        PlayerPrefs.SetInt("Level" + _levelNumber + "Minute", (int)Time.timeSinceLevelLoad / 60);
         PlayerPrefs.Save();
         Records.LoadSetting();
     }
